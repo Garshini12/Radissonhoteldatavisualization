313020b843730bf253868e6992aeb4dfc8ec41f7
Garshini12 <129948541+Garshini12@users.noreply.github.com>
2023-05-03 20:52:19 +0530

    Add files via upload

---
+++ Dashboard radisson.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+lement, lement, vizElement);                </script>vizElement);                </script>
+if __name__ == '__main__':
+    app.run(debug=True)
